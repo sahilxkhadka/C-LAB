de4ec175692d935a94704ad4ac0b8ef810eb6385
stormraider2059 <sahilkhadka767@gmail.com>
2022-03-12 20:26:39 +0545

    lab 5 final

--- lab5/lab5q1.cpp
+++ lab5/lab5q1.cpp
@@ -1,3 +1,16 @@
+// (1) Write a program to simulate banking transactions of 3 users. The program must be menu driven. Include the following operations in the menu: 
+
+// 1. Deposit  
+
+// 2. Withdraw 
+
+// 3. Display Balance 
+
+// 4. Display Total Operations 
+
+// 5. Exit 
+
+// Use a static member function to display total number of operations (option 4) performed through the application.  
 #include <iostream>
 using namespace std;
 
@@ -6,9 +19,9 @@ class Transactions{
 	static int count;		
 	public:
 		void depositMoney() {
-		cout << "Enter the amount of money you want to deposit:" << endl;
-		cin >> depositAmount;
-		count++;
+			cout << "Enter the amount of money you want to deposit:" << endl;
+			cin >> depositAmount;
+			count++;
 		}
 		void withdrawMoney(){
 			do {
@@ -17,13 +30,13 @@ class Transactions{
 			} while (depositAmount < withdrawAmount);
 			balance = depositAmount - withdrawAmount;
 			count++;
-	}
-	void displayBalance() {
-		cout << "Your Balance = " << balance;
-	}
-	void displayTransactions() {
-		cout << "The total no. of transactions done today = " << count << endl;
-	}
+		}
+		void displayBalance() {
+			cout << "Your Balance = " << balance<<endl;
+		}
+		static void displayTransactions() {
+			cout << "The total no. of transactions done today = " << count << endl;
+		}
 };
 int Transactions::count;
 
@@ -31,7 +44,7 @@ int main() {
 	Transactions user[3];
 	for (int i = 0; i < 3; i++) {
 		int activeUser = 1, menu;
-		cout<<"Greetings to our valued user 00"<<i + 1;
+		cout<<"Greetings to our valued user 00"<<i + 1<<endl;
 		do {
 			cout << "Enter the number of transaction you want to perform as seen in the menu" << endl;
 			cin >> menu;
@@ -46,7 +59,7 @@ int main() {
 					user[i].displayBalance();
 					break;
 				case 4:
-					user[i].displayTransactions();
+					Transactions::displayTransactions();
 					break;
 				case 5:
 					activeUser = 0;
