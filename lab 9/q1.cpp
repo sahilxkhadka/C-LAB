819c27ddfd05d32bf5361eccf273cd3899dae367
Sahil Khadka <sahilkhadka767@gmail.com>
2022-04-03 07:05:18 +0545

    lab9

---
+++ lab 9/q1.cpp
@@ -0,0 +1,28 @@
+//  Write a program that overloads the binary arithmetic '/' operator using a friend function.
+#include<iostream>
+using namespace std;
+
+class Number {
+    private:
+        float x;
+    public: 
+        Number(float x) {
+            this->x = x;
+        }
+        friend float operator / (Number, Number);
+        void display() {
+            cout<<"The value of x = "<<x<<endl;
+        }
+};
+
+float operator / (Number n1, Number n2) {
+    return n1.x / n2.x;
+}
+
+int main (){
+    Number num1(9), num2(5);
+    float res = num1 / num2;
+    Number num3(res);
+    num3.display();
+    return 0;
+}
---
+++ lab 9/q2.cpp
@@ -0,0 +1,30 @@
+// (2) Write an object oriented program to overload the Pointer-To-Member (->) operator. 
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Details {
+    private:
+        string firstName;
+        string secondName;
+    public:
+        void setData() {
+            cout<<"Enter your first name:"<<endl;
+            cin>>firstName;
+            cout<<"Enter your second name:"<<endl;
+            cin>>secondName;
+        }
+        void display() {
+            cout<<endl<<"Your full name is: "<<firstName<<" "<<secondName<<endl; 
+        }
+        Details* operator ->()  {
+            return this;
+        }
+};
+
+int main (){
+    Details sahil;
+    sahil->setData();
+    sahil->display();
+    return 0;
+}
